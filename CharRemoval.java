aed184755621e5558ff376e50e542c8b8562407b
aishasusan <78200464+aishasusan@users.noreply.github.com>
2023-04-26 09:44:45 +0530

    Function to remove character from String in Java
    This program will take a String as an Input and also a character as an Input.
    This input is a character that we want to remove from a given String.
    For Example:  I have given a String “Quescol” as an input and also a character ‘e’ as an input.
    We will get Output as “Quscol”.

---
+++ CharRemoval.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+public class CharRemoval {  
+    public static void main(String[] args) {  
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Please give a String : ");  
+        String str= sc.nextLine();  
+        Scanner sc1= new Scanner(System.in);
+        System.out.print("Please give a Char : ");  
+        String ch= sc1.nextLine();
+        System.out.println(charToRemove(str, ch));
+    }  
+    public static String charToRemove(String str, String ch){
+        return str.replace(ch,"");
+    }
+} 
